982486b36b7186978ec6c06a4eec1b43640b124a
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-07-27 22:59:24 -0300

    feat(api): implement interceptor for bearer and logout service

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -1,18 +1,22 @@
 import axios from 'axios'
+import { useAuthStore } from '@store'
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
 })
 
-// Interceptor for bearer token and basic authentication
-// api.interceptors.request.use((config) => {
-//   const username =
-//     process.env.NEXT_PUBLIC_SCHOOL_GUARDIAN_AUTHENTICATOR_USERNAME
-//   const password =
-//     process.env.NEXT_PUBLIC_SCHOOL_GUARDIAN_AUTHENTICATOR_PASSWORD
-//   const credentials = `${username}:${password}`
-//   const encondeCredentials = Buffer.from(credentials).toString('base64')
 
-//   config.headers.Authorization = `Basic ${encondeCredentials}`
-//   return config
-// })
+api.interceptors.request.use((config) => {
+  const { data } = useAuthStore.getState()
+  const token = data.accessToken.token
+  const username =
+    process.env.NEXT_PUBLIC_AUTHENTICATOR_USERNAME
+  const password =
+    process.env.NEXT_AUTHENTICATOR_PASSWORD
+  const credentials = `${username}:${password}`
+  const encondeCredentials = Buffer.from(credentials).toString('base64')
+
+  config.headers.Authorization =
+    token !== '' ? `Bearer ${token}` : `Basic ${encondeCredentials}`
+  return config
+})
--- src/services/http/auth-services.ts
+++ src/services/http/auth-services.ts
@@ -1,6 +1,6 @@
 import { api } from '@/services'
 import type { LoginProps, ContentLogin } from '@/services/types'
-import type { DefaultResponseWithContent } from '@/@types/base-types'
+import type { DefaultResponseWithContent,DefaultResponse } from '@/@types/base-types'
 import { browserName } from 'react-device-detect'
 
 export const authServices = {
@@ -15,5 +15,11 @@ export const authServices = {
       body,
     )
     return response.data.content
+  },
+  logout: async () => {
+    const response = await api.post<DefaultResponse>(
+      'v1/auth/logout',
+    )
+    return response.data
   },
 }
