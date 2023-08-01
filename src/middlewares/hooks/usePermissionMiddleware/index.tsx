7d7687f18e0ce68abdb0916e494cbf214f4d7ca1
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-08-01 18:46:57 -0300

    feat(change permissions middleware): change

--- src/middlewares/hooks/usePermissionMiddleware/index.tsx
+++ src/middlewares/hooks/usePermissionMiddleware/index.tsx
@@ -1,17 +1,18 @@
 'use client'
-import { usePermissions } from '@hooks'
-import { usePathname, notFound } from 'next/navigation'
-import type { Paths } from '@/@types/base-types'
+import { useNavigation, usePermissions } from '@hooks'
+import { usePathname} from 'next/navigation'
 import { useEffect } from 'react'
+import type { Paths } from '@/@types/base-types'
 
 export function usePermissionMiddleware() {
+  const {navigate} = useNavigation()
   const { checkPermissionPerUrl } = usePermissions()
   const pathName = usePathname()
 
   const hasPermission = checkPermissionPerUrl(pathName as Paths)
 
   useEffect(() => {
-    if (!hasPermission) notFound()
+    if (!hasPermission) navigate('/not-found')
   }, [pathName])
 
   return {
